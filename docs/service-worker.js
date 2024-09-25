4d2fcccf1a12524a2d52f60af16f026b8f76eb0d
sukalaper <anggirrr31@gmail.com>
2024-09-25 18:05:21 +0700

    feat: add localStorage to keep dark mode

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -4,7 +4,6 @@ self.addEventListener('install', (event) => {
       return cache.addAll([
         '/',
         'docs/',
-        // tambahkan file lainnya yang perlu dicache
       ]);
     })
   );
