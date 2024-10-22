b2d6dd813ccb6ebd7f41787f48f87161b252ce1d
sukalaper <anggirrr31@gmail.com>
2024-10-22 09:20:12 +0700

    rebuilding site Tue Oct 22 09:20:12 AM WIB 2024

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -9,6 +9,18 @@ self.addEventListener('install', (event) => {
 
   const blogPosts = [
     'p/01.html',
+    'p/02.html',
+    'p/03.html',
+    'p/04.html',
+    'p/05.html',
+    'p/06.html',
+    'p/07.html',
+    'p/08.html',
+    'p/09.html',
+    'p/10.html',
+    'p/11.html',
+    'p/12.html',
+    'p/13.html',
   ];
 
   event.waitUntil(
